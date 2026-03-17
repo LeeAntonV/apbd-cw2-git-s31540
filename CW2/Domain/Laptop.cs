c795cb26f214fb2d722ba1edbc0bfdeffda0b11c
Anton <koshkai2005@gmail.com>
2026-03-17 11:14:17 +0100

    Laptop class

--- CW2/Domain/Laptop.cs
+++ CW2/Domain/Laptop.cs
@@ -1,6 +1,22 @@
-﻿namespace CW2.Domain;
-
-public class Laptop
+﻿namespace CW2.Domain
 {
-    
+    public class Laptop : Equipment
+    {
+        public Laptop(string name, string brand, int ramGb, string cpuModel)
+            : base(name, name)
+        {
+            Brand = brand;
+            RamGb = ramGb;
+            CpuModel = cpuModel;
+        }
+
+        public string Brand { get; }
+        public int RamGb { get; }
+        public string CpuModel { get; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Brand: {Brand} | RAM: {RamGb} GB | CPU: {CpuModel}";
+        }
+    }
 }
