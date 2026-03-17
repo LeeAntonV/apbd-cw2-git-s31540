5111849b7ff3924b9e2ef0af483327a24b6e7dfc
Anton <koshkai2005@gmail.com>
2026-03-17 15:16:49 +0100

    Data/StorageModel

--- CW2/Data/StorageModel.cs
+++ CW2/Data/StorageModel.cs
@@ -1,6 +1,10 @@
-﻿namespace CW2.Data;
+﻿using CW2.Domain;
+
+namespace CW2.Data;
 
 public class StorageModel
 {
-    
+    public List<User> Users { get; set; } = new();
+    public List<Equipment> Equipment { get; set; } = new();
+    public List<Rental> Rentals { get; set; } = new();
 }
