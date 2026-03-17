a4b3413a89518a53ece9bb7d605578dc8bdbbb41
Anton <koshkai2005@gmail.com>
2026-03-17 11:35:03 +0100

    Rental class

---
+++ CW2/Domain/Rental.cs
@@ -0,0 +1,56 @@
+﻿namespace CW2.Domain;
+
+public class Rental
+{
+    private static int _nextId = 1;
+
+    public int Id { get; }
+    public User User { get; }
+    public Equipment Equipment { get; }
+    public DateTime RentalDate { get; }
+    public DateTime DueDate { get; }
+    public DateTime? ReturnDate { get; private set; }
+    public decimal Penalty { get; private set; }
+
+    public bool IsActive => ReturnDate == null;
+    public bool IsReturned => ReturnDate != null;
+    public bool WasReturnedOnTime => ReturnDate != null && ReturnDate.Value.Date <= DueDate.Date;
+
+    public Rental(User user, Equipment equipment, DateTime rentalDate, int rentalDays)
+    {
+        Id = _nextId++;
+        User = user;
+        Equipment = equipment;
+        RentalDate = rentalDate;
+        DueDate = rentalDate.AddDays(rentalDays);
+        ReturnDate = null;
+        Penalty = 0;
+    }
+
+    public bool IsOverdue(DateTime currentDate)
+    {
+        return IsActive && currentDate.Date > DueDate.Date;
+    }
+
+    public void ReturnEquipment(DateTime returnDate, decimal penalty)
+    {
+        if (IsReturned)
+        {
+            throw new InvalidOperationException("This rental has already been returned.");
+        }
+
+        ReturnDate = returnDate;
+        Penalty = penalty;
+    }
+
+    public override string ToString()
+    {
+        string returnText = ReturnDate.HasValue
+            ? ReturnDate.Value.ToString("yyyy-MM-dd")
+            : "Not returned";
+
+        return $"Rental Id: {Id}, User: {User.FirstName} {User.LastName}, Equipment: {Equipment.Name}, " +
+               $"Rental Date: {RentalDate:yyyy-MM-dd}, Due Date: {DueDate:yyyy-MM-dd}, " +
+               $"Return Date: {returnText}, Penalty: {Penalty}";
+    }
+}
--- CW2/obj/Debug/net10.0/CW2.AssemblyInfo.cs
+++ CW2/obj/Debug/net10.0/CW2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CW2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e466c91c7809cf95aef80b94b60b5e86e8c4d17f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b90e5681644d453218ec4a899a55f5c205141d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CW2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CW2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
