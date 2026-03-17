e466c91c7809cf95aef80b94b60b5e86e8c4d17f
Anton <koshkai2005@gmail.com>
2026-03-17 11:15:35 +0100

    Projector class

--- CW2/Domain/Projector.cs
+++ CW2/Domain/Projector.cs
@@ -1,6 +1,23 @@
-﻿namespace CW2.Domain;
-
-public class Projector
+﻿namespace CW2.Domain
 {
-    
+    public class Projector : Equipment
+    {
+        public Projector(string name, int brightnessLumens, string resolution, string technology)
+            : base(name, name)
+        {
+            BrightnessLumens = brightnessLumens;
+            Resolution = resolution;
+            Technology = technology;
+        }
+
+        public int BrightnessLumens { get; }
+        public string Resolution { get; }
+        public string Technology { get; }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                   $" | Brightness: {BrightnessLumens} lm | Resolution: {Resolution} | Technology: {Technology}";
+        }
+    }
 }
