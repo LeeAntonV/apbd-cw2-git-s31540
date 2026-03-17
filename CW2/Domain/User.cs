d8523d615e1975812a779586bfc83847244997db
Anton <koshkai2005@gmail.com>
2026-03-17 11:28:31 +0100

    User class

--- CW2/Domain/User.cs
+++ CW2/Domain/User.cs
@@ -1,6 +1,20 @@
-﻿namespace CW2.Domain;
+﻿using CW2.Enum;
 
-public class User
+namespace CW2.Domain;
+
+public abstract class User
 {
-    
+    public static int _nextId = 1;
+    public int Id { get; }
+    public string FirstName { get; }
+    public string LastName { get; }
+    public UserType UserType { get; }
+
+    protected User(string id, string firstName, string lastName, UserType userType)
+    {
+        Id = _nextId++;
+        FirstName = firstName;
+        LastName = lastName;
+        UserType = userType;
+    }
 }
